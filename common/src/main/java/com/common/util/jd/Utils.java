40fa683ca5378fd3fd25e56c5fe2258563ae1c69
zhaofei2018 <2661627633@qq.com>
2020-04-02 09:00:56 +0800

    1

--- common/src/main/java/com/common/util/jd/Utils.java
+++ common/src/main/java/com/common/util/jd/Utils.java
@@ -764,7 +764,7 @@ public class Utils {
    * @return
    */
   public static boolean judgeIsTaoBao(String msg) {
-    if (msg.contains("https://u.jd.com/")) {
+    if (msg.contains("https://u.jd.com/") || msg.contains("https://coupon")) {
       return false;
     } else {
       return true;
