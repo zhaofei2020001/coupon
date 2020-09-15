af55dd9904d0d32cde6d468fc1823bfbfc782858
zhaofei2018 <2661627633@qq.com>
2020-09-15 12:56:45 +0800

    1

--- jd-coupon/src/main/java/com/jd/coupon/service/JdService.java
+++ jd-coupon/src/main/java/com/jd/coupon/service/JdService.java
@@ -113,10 +113,10 @@ public class JdService {
 
                         //将转链后的线报发送到 配置的群中
                         List<String> finalImg_text = img_text;
-//
-//                            WechatSendMsgDto wechatSendMsgDto = new WechatSendMsgDto(AllEnums.loveCatMsgType.PRIVATE_MSG.getCode(), robotId, accout.getGroupId(), finalImg_text.get(0), null, null, null);
-//                            String s1 = WechatUtils.sendWechatTextMsg(wechatSendMsgDto);
-//                            log.info("发送文字线报结果----->:{}", s1);
+
+                            WechatSendMsgDto wechatSendMsgDto = new WechatSendMsgDto(AllEnums.loveCatMsgType.PRIVATE_MSG.getCode(), robotId, accout.getGroupId(), finalImg_text.get(0), null, null, null);
+                            String s1 = WechatUtils.sendWechatTextMsg(wechatSendMsgDto);
+                            log.info("发送文字线报结果----->:{}", s1);
 
                             try {
                                 Thread.sleep(2000L);
@@ -125,10 +125,10 @@ public class JdService {
                             }
 
                             if (StringUtils.isNotBlank(finalImg_text.get(1))) {
-//                                //发送图片
-//                                WechatSendMsgDto wechatSendMsgDto_img = new WechatSendMsgDto(AllEnums.loveCatMsgType.SKU_PICTURE.getCode(), robotId, accout.getGroupId(), finalImg_text.get(1), null, null, null);
-//                                String s2 = WechatUtils.sendWechatTextMsg(wechatSendMsgDto_img);
-//                                log.info("发送图片结果信息--------------->:{}", s2);
+                                //发送图片
+                                WechatSendMsgDto wechatSendMsgDto_img = new WechatSendMsgDto(AllEnums.loveCatMsgType.SKU_PICTURE.getCode(), robotId, accout.getGroupId(), finalImg_text.get(1), null, null, null);
+                                String s2 = WechatUtils.sendWechatTextMsg(wechatSendMsgDto_img);
+                                log.info("发送图片结果信息--------------->:{}", s2);
                             } else {
                                 log.info("图片为空,不发送----->");
                             }
