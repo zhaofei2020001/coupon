3915e68fd5d09152ebf3fa501977a655b3a7d3c5
zhaofei2018 <2661627633@qq.com>
2020-09-23 12:18:54 +0800

    1

--- common/src/main/java/com/common/util/jd/Utils.java
+++ common/src/main/java/com/common/util/jd/Utils.java
@@ -536,7 +536,7 @@ public class Utils {
 
                 if (it.equals("1元") && (msg.contains(".1元") || msg.contains("1元/"))) {
 
-                } else if (it.equals("秒杀") && (msg.contains("秒杀价") || msg.contains("秒杀 价"))) {
+                } else if (it.equals("秒杀") && (msg.contains("秒杀价") || msg.contains("秒杀 价")||msg.contains("秒 杀 价"))) {
 
                 } else {
                     log.info("关键字--->{},原消息--->{}", it, receiveMsgDto);
