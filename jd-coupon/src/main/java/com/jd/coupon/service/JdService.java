2ccb53e9b064515ad10f2d5c12ccc71466b4e897
zhaofei2018 <2661627633@qq.com>
2020-10-29 11:14:51 +0800

    1

--- jd-coupon/src/main/java/com/jd/coupon/service/JdService.java
+++ jd-coupon/src/main/java/com/jd/coupon/service/JdService.java
@@ -151,6 +151,7 @@ public class JdService {
                                 WechatSendMsgDto wechatSendMsgDto_img = new WechatSendMsgDto(AllEnums.loveCatMsgType.SKU_PICTURE.getCode(), robotId, accout.getGroupId(), havePicUrlAdd.get(), null, null, null);
                                 String s2 = WechatUtils.sendWechatTextMsg(wechatSendMsgDto_img);
                                 log.info("{}====>发送图片结果信息--------------->:{}", accout.getName(), s2);
+
                             } else {
                                 log.info("{}====>,图片为空,不发送----->", accout.getName());
                             }
