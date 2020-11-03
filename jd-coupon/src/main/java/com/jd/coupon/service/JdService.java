8ea366140109b14fcf3008385c80fb6fcddb8d16
zhaofei2018 <2661627633@qq.com>
2020-11-03 16:56:46 +0800

    1

--- jd-coupon/src/main/java/com/jd/coupon/service/JdService.java
+++ jd-coupon/src/main/java/com/jd/coupon/service/JdService.java
@@ -167,6 +167,7 @@ public class JdService {
                                 } catch (UnsupportedEncodingException e) {
                                     e.printStackTrace();
                                 }
+                                return;
                             }
                         }
 
