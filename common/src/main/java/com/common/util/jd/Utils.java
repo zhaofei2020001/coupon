3733e280d129a66227a0e00a2aaf9021d614ee14
zhaofei2018 <2661627633@qq.com>
2020-12-13 10:58:53 +0800

    1

--- common/src/main/java/com/common/util/jd/Utils.java
+++ common/src/main/java/com/common/util/jd/Utils.java
@@ -597,7 +597,7 @@ public class Utils {
             //===========将特价消息发送给群主===========
             accout.getMsgToPersons().forEach(it -> {
                 try {
-                    WechatSendMsgDto wechatSendMsgDto = new WechatSendMsgDto(AllEnums.loveCatMsgType.PRIVATE_MSG.getCode(), "wxid_8sofyhvoo4p322", it, URLEncoder.encode(removestr, "UTF-8"), null, null, null);
+                    WechatSendMsgDto wechatSendMsgDto = new WechatSendMsgDto(AllEnums.loveCatMsgType.PRIVATE_MSG.getCode(), "wxid_8sofyhvoo4p322", it, URLEncoder.encode(removestr.replaceAll("\\n",""), "UTF-8"), null, null, null);
                     WechatUtils.sendWechatTextMsg(wechatSendMsgDto);
                     log.info("发送111========>");
                 } catch (UnsupportedEncodingException e) {
@@ -608,7 +608,7 @@ public class Utils {
 
             //将转链后的线报发送到 配置的群中
             try {
-                WechatSendMsgDto wechatSendMsgDto = new WechatSendMsgDto(AllEnums.loveCatMsgType.PRIVATE_MSG.getCode(), "wxid_8sofyhvoo4p322", accout.getGroupId(), URLEncoder.encode(removestr, "UTF-8"), null, null, null);
+                WechatSendMsgDto wechatSendMsgDto = new WechatSendMsgDto(AllEnums.loveCatMsgType.PRIVATE_MSG.getCode(), "wxid_8sofyhvoo4p322", accout.getGroupId(), URLEncoder.encode(removestr.replaceAll("\\n",""), "UTF-8"), null, null, null);
                 WechatUtils.sendWechatTextMsg(wechatSendMsgDto);
                 log.info("发送222========>");
             } catch (UnsupportedEncodingException e) {
